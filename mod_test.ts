3912ee17da9bb22182531a92484a5d600db79ce4
Udit <udit@eskaytech.com>
2023-01-30 16:58:18 +0530

    Create a runtime extension wrapper for Blockless with Deno

---
+++ mod_test.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from "./test_deps.ts";
+import { CGIExtension } from "./mod.ts";
+
+Deno.test(function test_extension() {
+  const extension = new CGIExtension({
+    name: "Test Extension",
+    alias: "tester",
+    description: "This is a test extension",
+  });
+  
+  const encoder = new TextEncoder();
+  assertEquals(
+    extension.verify(),
+    encoder.encode(JSON.stringify({
+      alias: "tester",
+      name: "Test Extension",
+      description: "This is a test extension",
+      is_cgi: true,
+    })),
+  );
+});
